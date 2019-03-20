e691cec350e8576b911fa7e95ccc8cc53f8740e8
benjorah <bonwuorah@gmail.com>
2019-03-20 03:04:40 +0100

    home page started

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './tabs/tabs.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TabsComponent],
-  exports:[TabsComponent]
+  declarations: [TabsComponent, HeaderComponent],
+  exports:[TabsComponent,HeaderComponent]
 })
 export class SharedModule { }
