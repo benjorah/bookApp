03ec463d3d335b45ef73a0343a2537b7b1af332f
benjorah <bonwuorah@gmail.com>
2019-03-20 19:07:20 +0100

    added proper auth form events

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -7,6 +7,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  password:string="";
+  passwordCheck:string="";
+  email:string="";
+  passwordMismatch=false;
+
+  passwordFocused=false;
+  emailFocused=false;
+
+  passwordType="password";
+  passwordCheckType="password";
+
 
   constructor(
     private router:Router
@@ -22,4 +33,52 @@ export class SignUpComponent implements OnInit {
 
   }
 
+
+  comparePassword(r){
+    console.log(r);
+    if(this.password!=this.passwordCheck){
+      this.passwordMismatch=true;
+      return;
+    }
+
+    this.passwordMismatch=false;
+  }
+
+  focusChanged(input){
+    if(input.name=="email"){
+
+      this.emailFocused=true;
+
+    }
+
+    else if(input.name=="password"){
+      this.passwordFocused=true;
+    }
+  }
+
+  left(input){
+    if(input.name=="email"){
+
+      this.emailFocused=false;
+
+    }
+
+    else if(input.name=="password"){
+      this.passwordFocused=false;
+    }
+  }
+
+  togglepassword(input){
+
+    if(input.name=="password2"){
+
+      this.passwordCheckType=this.passwordCheckType=="text"?"password":"text";
+
+    }
+
+    else if(input.name=="password"){
+      this.passwordType=this.passwordType=="text"?"password":"text";
+    }
+
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './tabs/tabs.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TabsComponent, HeaderComponent],
-  exports:[TabsComponent,HeaderComponent]
+  declarations: [TabsComponent, HeaderComponent, SearchBarComponent],
+  exports:[TabsComponent,HeaderComponent,SearchBarComponent]
 })
 export class SharedModule { }
